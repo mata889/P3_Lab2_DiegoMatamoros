45bbaac0a12a31bd23eba5f5a11ad0229b1444fe
DiegoMatamoros <dam_900@unitec.edu>
2017-10-20 13:47:21 -0600

    Se ha creado un menu

---
+++ Lab2_P3DiegoMatamoros.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int menu;
+bool ciclo=true;
+do{
+  
+
+  cout<<"----MENU----"<<endl
+      <<"1.Ejercicio#1"<<endl
+      <<"2.Ejercicio#2"<<endl
+      <<"3.Ejercicio#3"<<endl
+      <<"4.Salir"<<endl
+      <<"Introduzca opcion:";
+  
+  cin>>menu;
+      
+    }while(menu!=4);
+return 0;  
+}
+
+
+
